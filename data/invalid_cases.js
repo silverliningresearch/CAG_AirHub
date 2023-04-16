7958e57fd7aa4229e555ad7ce6aef2195b44894e
lexuanbinh <lb@silverliningresearch.com>
2023-04-16 16:14:17 +0700

    update files

--- data/invalid_cases.js
+++ data/invalid_cases.js
@@ -1,6 +1,16 @@
     let removed_ids = `
     [
-      {"removed_id": "NONE"}
+{"removed_id": "16f1dfcd-60ab-4030-8ff4-195988f4f326"},
+{"removed_id": "06b15c9f-473b-4b99-ab1e-faa6358faad7"},
+{"removed_id": "3ed4189d-dede-4613-b4fe-9655f7d506c0"},
+{"removed_id": "2d47edac-15c7-49a3-8933-51635aba9742"},
+{"removed_id": "e90e3295-f085-4eaf-ad64-c3cc852247fe"},
+{"removed_id": "928f6497-d171-4e7e-bc48-c555216f09d1"},
+{"removed_id": "48dd5e17-ecef-4c48-91fe-7bef89094d03"},
+{"removed_id": "edc28b8b-5c0c-4111-8d52-71f995d6b812"},
+{"removed_id": "fa095ce4-5251-416c-8970-885fb4a09428"},
+{"removed_id": "811b4c2f-6e7f-4c2a-97a4-5914a702ef54"}
+
     ]
      
     `;
