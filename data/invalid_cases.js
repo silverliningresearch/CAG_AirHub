e0bbd0cc4aaa691c8e5d4ace91c31874849da8dc
lexuanbinh <lb@silverliningresearch.com>
2023-04-21 15:35:48 +0700

    update files

--- data/invalid_cases.js
+++ data/invalid_cases.js
@@ -1,7 +1,8 @@
     let removed_ids = `
     [
 {"removed_id": "16f1dfcd-60ab-4030-8ff4-195988f4f326"},
-{"removed_id": "a115e64a-5a3c-470b-9a03-71108019b238"}
+{"removed_id": "a115e64a-5a3c-470b-9a03-71108019b238"},
+{"removed_id": "3df41106-3664-4c4a-9feb-36ea6b0a79fc"}
 
 
 
