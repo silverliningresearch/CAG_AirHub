ed9fcb61f80c6e287987e71b87b2f04c64703d9b
lexuanbinh <lb@silverliningresearch.com>
2023-04-24 11:08:28 +0700

    update files

--- data/invalid_cases.js
+++ data/invalid_cases.js
@@ -2,7 +2,9 @@
     [
 {"removed_id": "16f1dfcd-60ab-4030-8ff4-195988f4f326"},
 {"removed_id": "a115e64a-5a3c-470b-9a03-71108019b238"},
-{"removed_id": "3df41106-3664-4c4a-9feb-36ea6b0a79fc"}
+{"removed_id": "3df41106-3664-4c4a-9feb-36ea6b0a79fc"},
+{"removed_id": "81578821-e894-4931-8af6-14cad6a593fc"},
+{"removed_id": "a25cedf2-8b7a-4dee-a888-ad6e76585d0e"}
 
 
 
