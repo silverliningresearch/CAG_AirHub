4c4b1b406a8f5bba6d2b3819b8fe3a77dc797ea6
lexuanbinh <lb@silverliningresearch.com>
2024-10-31 15:59:01 +0700

    refreshed

---
+++ clean cag.bat
@@ -0,0 +1,9 @@
+
+rm -rf .git
+
+git init
+git add .
+git commit -m "refreshed"
+
+git remote add origin https://github.com/silverliningresearch/CAG_AirHub.git
+git push -u --force origin <main>
---
+++ commit data.bat
@@ -0,0 +1,11 @@
+cd D:\My Data\01. My Works\01. Data Solutions\01. Projects\01. Silver Research\03. CAG\02. Source code\02. Search_Engine\CAG_AirHub
+
+D:
+
+git add .
+
+git commit -m "update files"
+
+git push --all
+
+cd ..
