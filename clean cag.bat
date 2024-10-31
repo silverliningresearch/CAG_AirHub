cb2b8809195e961a4ccbf1d6fef1fc23b6191839
lexuanbinh <lb@silverliningresearch.com>
2024-10-31 16:08:42 +0700

    refreshed

--- clean cag.bat
+++ clean cag.bat
@@ -5,7 +5,7 @@ git init
 git add .
 git commit -m "refreshed"
 
-git remote add origin2 https://github.com/silverliningresearch/CAG_AirHub.git
-git push -u --force origin2
+git remote add origin3 https://github.com/silverliningresearch/CAG_AirHub.git
+git push -u --force origin3 main
 
 pause
