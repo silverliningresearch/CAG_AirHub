1777fb75d78763a8114ba346662515d4546d3f50
lexuanbinh <lb@silverliningresearch.com>
2024-11-22 10:50:42 +0700

    update files

--- clean cag.bat
+++ clean cag.bat
@@ -1,11 +1,15 @@
+cd CAG_AirHub
 
 rm -rf .git
 
+git config --global init.defaultBranch main
+
 git init
 git add .
-git commit -m "refreshed"
+git commit -m "refresh"
 
-git remote add origin3 https://github.com/silverliningresearch/CAG_AirHub.git
-git push -u --force origin3 master
+git remote add origin https://github.com/silverliningresearch/CAG_AirHub.git
+git push -u --force origin main
+
 
 pause
